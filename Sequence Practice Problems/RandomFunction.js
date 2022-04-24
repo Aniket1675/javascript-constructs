33f32c2d5dc30bc9c8a6baac28db69757af65fd4
Aniket1675 <Aniket@05>
2022-04-24 13:57:53 +0530

    ADDED: Ramdom function program using javascript

---
+++ Sequence Practice Problems/RandomFunction.js
@@ -0,0 +1,26 @@
+/**
+ * Question 1: Randomn Function to get Single Digit
+*/
+
+console.log(Math.floor(Math.random() * 10));
+console.log("----------------------------------");
+
+
+/************************************************************************************************ */
+//Question 2: Randomn to get number between 1 and 6
+
+console.log(Math.floor(Math.random() * 6 + 1));
+console.log("-----------------------------------");
+
+/************************************************************************************************ */
+
+//Question 3: Add 2 random dice numbers
+
+let number1 = Math.floor(Math.random() * 6 + 1)
+console.log("The number of dice 1 is " + number1);
+let number2 = Math.floor(Math.random() * 6 + 1)
+console.log("The number of dice 2 is " + number2);
+let sum = number1 + number2;
+console.log("The sum of two dice is : " + sum);
+
+console.log("-----------------------------------");
